1f94a5cd42e276c1d91d75bbf7d38096d2667a7a
pyaephyoaung_paukgyi <pyaephyoaung@nearme.com.mm>
2025-03-18 15:16:48 +0630

    Add project files.

---
+++ WebApi/Program.cs
@@ -0,0 +1,43 @@
+using Application;
+using Microsoft.AspNetCore.Mvc;
+using Persistence;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var config = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json",false, true)
+                 .Build();
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+builder.Services.AddApplication();
+builder.Services.AddPersistence(config);
+#region API Versioning
+// Add API Versioning to the Project
+builder.Services.AddApiVersioning(config =>
+{
+    // Specify the default API Version as 1.0
+    config.DefaultApiVersion = new ApiVersion(1, 0);
+    // If the client hasn't specified the API version in the request, use the default API version number
+    config.AssumeDefaultVersionWhenUnspecified = true;
+    // Advertise the API versions supported for the particular endpoint
+    config.ReportApiVersions = true;
+});
+#endregion
+var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.MapScalarApiReference();
+app.MapControllers();
+
+app.Run();
